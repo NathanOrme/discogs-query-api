cf9ccfbe26647b338dfa5d733af4a45dcaee5048
Nathan Orme <nathan.orme23@gmail.com>
2024-08-30 15:19:09 +0100

    Added Javadoc

--- src/main/java/org/discogs/query/config/SpringFoxConfig.java
+++ src/main/java/org/discogs/query/config/SpringFoxConfig.java
@@ -7,8 +7,19 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+/**
+ * Configuration class for setting up Swagger documentation.
+ * This class configures Swagger to generate API documentation for the Discogs Query application.
+ */
 @Configuration
 public class SpringFoxConfig {
+
+    /**
+     * Creates a {@link Docket} bean to configure Swagger for the application.
+     * This bean sets up the Swagger documentation to include all APIs and paths in the application.
+     *
+     * @return a {@link Docket} instance configured for Swagger 2 documentation
+     */
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
--- src/main/java/org/discogs/query/exceptions/DiscogsAPIException.java
+++ src/main/java/org/discogs/query/exceptions/DiscogsAPIException.java
@@ -3,9 +3,19 @@ package org.discogs.query.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class for handling errors that occur when interacting with the Discogs API.
+ * This exception is used to signify an internal server error (HTTP 500) in the application.
+ */
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class DiscogsAPIException extends RuntimeException {
 
+    /**
+     * Constructs a new {@link DiscogsAPIException} with the specified detail message and cause.
+     *
+     * @param message the detail message for this exception
+     * @param e       the cause of this exception (a {@link Exception} that triggered this exception)
+     */
     public DiscogsAPIException(final String message, final Exception e) {
         super(message, e);
     }

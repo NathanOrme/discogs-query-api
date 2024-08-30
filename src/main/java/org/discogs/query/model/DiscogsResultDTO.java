f565858cf2f7def4d6e033a48b6b1db15ed6a2fa
Nathan Orme <nathan.orme23@gmail.com>
2024-08-30 11:57:54 +0100

    Implemented result

--- src/main/java/org/discogs/query/model/DiscogsResultDTO.java
+++ src/main/java/org/discogs/query/model/DiscogsResultDTO.java
@@ -1,20 +1,21 @@
 package org.discogs.query.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DiscogsResultDTO {
 
-    private String title;
-
+    private List<DiscogsEntryDTO> results;
 }

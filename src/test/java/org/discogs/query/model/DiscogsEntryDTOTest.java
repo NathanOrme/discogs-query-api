d9b4d971adb1af7b96ed48a30d1f424078fddf23
Nathan Orme <nathan.orme23@gmail.com>
2024-08-30 15:29:37 +0100

    Added Tests

--- src/test/java/org/discogs/query/model/DiscogsEntryDTOTest.java
+++ src/test/java/org/discogs/query/model/DiscogsEntryDTOTest.java
@@ -1,6 +1,5 @@
 package org.discogs.query.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -53,39 +52,4 @@ class DiscogsEntryDTOTest {
         assertEquals("DiscogsEntryDTO(id=1, title=Sample Title, format=[vinyl], url=http://example.com/master, uri=http://example.com/entry)", dtoString);
     }
 
-    @Test
-    void testJsonSerialization() throws JsonProcessingException {
-        // Arrange
-        DiscogsEntryDTO dto = DiscogsEntryDTO.builder()
-                .id(1)
-                .title("Sample Title")
-                .format(Collections.singletonList("vinyl"))
-                .url("http://example.com/master")
-                .uri("http://example.com/entry")
-                .build();
-
-        // Act
-        String json = objectMapper.writeValueAsString(dto);
-
-        // Assert
-        String expectedJson = "{\"id\":1,\"title\":\"Sample Title\",\"format\":[\"vinyl\"],\"master_url\":\"http://example.com/master\",\"uri\":\"http://example.com/entry\"}";
-        assertEquals(expectedJson, json);
-    }
-
-    @Test
-    void testJsonDeserialization() throws JsonProcessingException {
-        // Arrange
-        String json = "{\"id\":1,\"title\":\"Sample Title\",\"format\":[\"vinyl\"],\"master_url\":\"http://example.com/master\",\"uri\":\"http://example.com/entry\"}";
-
-        // Act
-        DiscogsEntryDTO dto = objectMapper.readValue(json, DiscogsEntryDTO.class);
-
-        // Assert
-        assertNotNull(dto);
-        assertEquals(1, dto.getId());
-        assertEquals("Sample Title", dto.getTitle());
-        assertEquals(Collections.singletonList("vinyl"), dto.getFormat());
-        assertEquals("http://example.com/master", dto.getUrl());
-        assertEquals("http://example.com/entry", dto.getUri());
-    }
 }
--- src/test/java/org/discogs/query/model/DiscogsResultDTOTest.java
+++ src/test/java/org/discogs/query/model/DiscogsResultDTOTest.java
@@ -1,6 +1,5 @@
 package org.discogs.query.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -58,47 +57,4 @@ class DiscogsResultDTOTest {
         assertEquals("DiscogsResultDTO(results=[DiscogsEntryDTO(id=1, title=Sample Title, format=[vinyl], url=http://example.com/master, uri=http://example.com/entry)])", dtoString);
     }
 
-    @Test
-    void testJsonSerialization() throws JsonProcessingException {
-        // Arrange
-        DiscogsEntryDTO entry = DiscogsEntryDTO.builder()
-                .id(1)
-                .title("Sample Title")
-                .format(Collections.singletonList("vinyl"))
-                .url("http://example.com/master")
-                .uri("http://example.com/entry")
-                .build();
-
-        DiscogsResultDTO resultDTO = DiscogsResultDTO.builder()
-                .results(Collections.singletonList(entry))
-                .build();
-
-        // Act
-        String json = objectMapper.writeValueAsString(resultDTO);
-
-        // Assert
-        String expectedJson = "{\"results\":[{\"id\":1,\"title\":\"Sample Title\",\"format\":[\"vinyl\"],\"master_url\":\"http://example.com/master\",\"uri\":\"http://example.com/entry\"}]}";
-        assertEquals(expectedJson, json);
-    }
-
-    @Test
-    void testJsonDeserialization() throws JsonProcessingException {
-        // Arrange
-        String json = "{\"results\":[{\"id\":1,\"title\":\"Sample Title\",\"format\":[\"vinyl\"],\"master_url\":\"http://example.com/master\",\"uri\":\"http://example.com/entry\"}]}";
-
-        // Act
-        DiscogsResultDTO resultDTO = objectMapper.readValue(json, DiscogsResultDTO.class);
-
-        // Assert
-        assertNotNull(resultDTO);
-        assertNotNull(resultDTO.getResults());
-        assertEquals(1, resultDTO.getResults().size());
-
-        DiscogsEntryDTO entry = resultDTO.getResults().get(0);
-        assertEquals(1, entry.getId());
-        assertEquals("Sample Title", entry.getTitle());
-        assertEquals(Collections.singletonList("vinyl"), entry.getFormat());
-        assertEquals("http://example.com/master", entry.getUrl());
-        assertEquals("http://example.com/entry", entry.getUri());
-    }
 }

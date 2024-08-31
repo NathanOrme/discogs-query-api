1b7670195e8eeedbde5d699d83f0749065f53deb
Nathan Orme <nathan.orme23@gmail.com>
2024-08-31 20:57:56 +0100

    Moved packages around

--- src/main/java/org/discogs/query/client/limits/package-info.java
+++ src/main/java/org/discogs/query/client/limits/package-info.java
@@ -9,7 +9,8 @@
  * <p>
  * Classes and interfaces in this package include:
  * <ul>
- *     <li>{@link org.discogs.query.client.limits.RateLimiter} - An interface for implementing rate limiting strategies.</li>
+ *     <li>{@link org.discogs.query.client.limits.RateLimiter} -
+ *     An interface for implementing rate limiting strategies.</li>
  * </ul>
  */
 package org.discogs.query.client.limits;

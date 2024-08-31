34b8db6b09891d5eac3b8bc78afcb363875b19a8
Nathan Orme <nathan.orme23@gmail.com>
2024-08-31 14:36:40 +0100

    Security config change

--- src/main/java/org/discogs/query/config/SecurityConfig.java
+++ src/main/java/org/discogs/query/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.discogs.query.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -51,9 +50,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated() // Require authentication for all requests
+                        .anyRequest().permitAll()
                 )
-                .httpBasic(Customizer.withDefaults()) // Use basic HTTP authentication
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Stateless session management
         return http.build();

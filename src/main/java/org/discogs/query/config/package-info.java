c9ad70964914a53bd6c9db677af36a1e9a71b1ce
Nathan Orme <nathan.orme23@gmail.com>
2024-08-31 09:47:06 +0100

    Enhanced Javadoc

--- src/main/java/org/discogs/query/config/package-info.java
+++ src/main/java/org/discogs/query/config/package-info.java
@@ -4,13 +4,18 @@
  * This package includes various configuration settings necessary for the proper functioning of the application.
  * It encompasses:
  * <ul>
- *     <li>Security configurations, defining how security is managed and applied across the application.</li>
- *     <li>Web configurations, such as CORS settings, to handle cross-origin requests and other web-specific settings.</li>
- *     <li>General application configurations that might affect the behavior of the application context.</li>
+ *     <li>Security configurations, defining how security
+ *     is managed and applied across the application.</li>
+ *     <li>Web configurations, such as CORS settings,
+ *     to handle cross-origin requests and other web-specific settings.</li>
+ *     <li>General application configurations that
+ *     might affect the behavior of the application context.</li>
  * </ul>
  * <p>
- * The classes in this package are responsible for setting up and customizing the application's environment
- * to ensure secure, efficient, and reliable operation. They work together to define how different aspects of the application
+ * The classes in this package are responsible for setting up
+ * and customizing the application's environment
+ * to ensure secure, efficient, and reliable operation.
+ * They work together to define how different aspects of the application
  * interact with each other and with external systems.
  */
 package org.discogs.query.config;
--- src/main/java/org/discogs/query/model/package-info.java
+++ src/main/java/org/discogs/query/model/package-info.java
@@ -1,14 +1,18 @@
 /**
  * Contains model classes used for representing data in the Discogs Query application.
  * <p>
- * This package includes classes that define the structure and format of data for various purposes within the application.
+ * This package includes classes that define the structure
+ * and format of data for various purposes within the application.
  * It includes:
  * <ul>
- *     <li>Data Transfer Objects (DTOs) for transferring data between different layers of the application.</li>
+ *     <li>Data Transfer Objects (DTOs) for transferring
+ *     data between different layers of the application.</li>
  * </ul>
  * <p>
  * The classes in this package are designed to encapsulate and manage data related to Discogs queries and results,
- * including the representation of query parameters, search results, and other relevant information. These models
- * facilitate interaction with the API and ensure that data is handled consistently throughout the application.
+ * including the representation of query parameters,
+ * search results, and other relevant information. These models
+ * facilitate interaction with the API and ensure
+ * that data is handled consistently throughout the application.
  */
 package org.discogs.query.model;

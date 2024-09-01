5da8eb48d43ecba4e68ded10663c224680d38952
Nathan Orme <nathan.orme23@gmail.com>
2024-09-01 10:54:59 +0100

    Introduced Caffeine pt3

--- src/main/java/org/discogs/query/config/CacheConfig.java
+++ src/main/java/org/discogs/query/config/CacheConfig.java
@@ -8,9 +8,29 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Configuration class for setting up caching in the application.
+ * <p>
+ * This class is annotated with {@link Configuration},
+ * indicating that it provides Spring configuration.
+ * It defines a {@link CacheManager} bean that uses Caffeine for caching.
+ * </p>
+ */
 @Configuration
 public class CacheConfig {
 
+    /**
+     * Creates and configures a {@link CacheManager} bean using Caffeine.
+     * <p>
+     * This method sets up a {@link CaffeineCacheManager} with specific cache settings:
+     * <ul>
+     *     <li>Cache entries will expire 10 minutes after they are written.</li>
+     *     <li>The cache will have a maximum size of 1000 entries.</li>
+     * </ul>
+     * </p>
+     *
+     * @return a configured {@link CacheManager} instance
+     */
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();

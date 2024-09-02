50839a8b8c41a5ef20ae3a82426563d8a3793db2
Nathan Orme <nathan.orme23@gmail.com>
2024-09-02 19:50:51 +0100

    Added javadoc

--- src/main/java/org/discogs/query/domain/release/ExtraArtist.java
+++ src/main/java/org/discogs/query/domain/release/ExtraArtist.java
@@ -5,16 +5,51 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents an extra artist involved in a release. These could be
+ * additional performers, producers, or other contributors not considered
+ * the main artist(s).
+ */
 @Data
 @NoArgsConstructor
 @JsonDeserialize(as = ExtraArtist.class)
 public class ExtraArtist {
+
+    /**
+     * The artist name variation (ANV) used on the release. This could be
+     * an alternate spelling, alias, or pseudonym.
+     */
     private String anv;
+
+    /**
+     * The unique identifier of the extra artist.
+     */
     private int id;
+
+    /**
+     * The joining phrase or term, if the artist is part of a group
+     * (e.g., "feat.", "vs", etc.).
+     */
     private String join;
+
+    /**
+     * The name of the extra artist as it appears on the release.
+     */
     private String name;
+
+    /**
+     * The resource URL pointing to the extra artist's page on Discogs.
+     */
     @JsonProperty("resource_url")
     private String resourceUrl;
+
+    /**
+     * The role of the extra artist in the release (e.g., "Vocals", "Producer", etc.).
+     */
     private String role;
+
+    /**
+     * The specific tracks the extra artist is credited on, if applicable.
+     */
     private String tracks;
 }
--- src/main/java/org/discogs/query/domain/release/Label.java
+++ src/main/java/org/discogs/query/domain/release/Label.java
@@ -5,15 +5,39 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a label involved in the release. Labels are companies or
+ * entities that publish or distribute the release.
+ */
 @Data
 @NoArgsConstructor
 @JsonDeserialize(as = Label.class)
 public class Label {
+
+    /**
+     * The catalog number assigned to the release by the label.
+     */
     private String catno;
+
+    /**
+     * The type of entity the label represents (e.g., "Label", "Company").
+     */
     @JsonProperty("entity_type")
     private String entityType;
+
+    /**
+     * The unique identifier of the label.
+     */
     private int id;
+
+    /**
+     * The name of the label.
+     */
     private String name;
+
+    /**
+     * The resource URL pointing to the label's page on Discogs.
+     */
     @JsonProperty("resource_url")
     private String resourceUrl;
 }

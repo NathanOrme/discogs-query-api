c29cd8794f8d0e1d6eacc891e18fda6cf5925ce2
Nathan Orme <nathan.orme23@gmail.com>
2024-09-02 18:28:27 +0100

    Added new validator

--- src/main/java/org/discogs/query/annotations/VariousArtistsValidation.java
+++ src/main/java/org/discogs/query/annotations/VariousArtistsValidation.java
@@ -8,17 +8,40 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to validate {@link org.discogs.query.model.DiscogsQueryDTO}
+ * objects for the "Various Artists" scenario.
+ * This annotation ensures that when the artist is
+ * identified as "Various Artists", both a track and an album
+ * must be supplied in the DTO.
+ */
 @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = VariousArtistsValidator.class)
 public @interface VariousArtistsValidation {
 
-    //error message
+    /**
+     * Default error message when the validation fails.
+     *
+     * @return the default error message
+     */
     String message() default "A track or album must be supplied if dealing with various artists";
 
-    //represents group of constraints
+    /**
+     * Groups of constraints that this annotation belongs to.
+     * This can be used to apply different sets of validation
+     * rules based on different contexts.
+     *
+     * @return the groups of constraints
+     */
     Class<?>[] groups() default {};
 
-    //represents additional information about annotation
+    /**
+     * Payload that can carry additional metadata about the annotation.
+     * This is typically used by validation clients
+     * to carry additional information about the validation constraint.
+     *
+     * @return the payload containing additional information
+     */
     Class<? extends Payload>[] payload() default {};
 }

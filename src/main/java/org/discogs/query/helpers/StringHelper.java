3bd983e462cab3856850e85c88091fa101e335a0
Nathan Orme <nathan.orme23@gmail.com>
2024-09-04 13:05:15 +0100

    Added new class and a boolean to tackle compilation album situation

---
+++ src/main/java/org/discogs/query/helpers/StringHelper.java
@@ -0,0 +1,42 @@
+package org.discogs.query.helpers;
+
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * A utility class that provides helper methods for
+ * String manipulation and validation.
+ * This class is annotated as a Spring Component,
+ * allowing it to be managed by Spring's
+ * dependency injection framework.
+ *
+ * <p>
+ * The primary function of this class is to check if a given string
+ * is neither null nor blank.
+ * <p>
+ * Example usage:
+ * <pre>
+ *     StringHelper stringHelper = new StringHelper();
+ *     boolean result = stringHelper.isNotNullOrBlank("example");
+ * </pre>
+ */
+@Component
+@NoArgsConstructor
+public class StringHelper {
+
+    /**
+     * Checks if the given string is not null and not blank.
+     *
+     * <p>
+     * A string is considered not blank if it
+     * contains at least one non-whitespace character.
+     *
+     * @param string the string to check
+     * @return {@code true} if the string is not null and contains at
+     * least one non-whitespace character,
+     * {@code false} otherwise
+     */
+    public boolean isNotNullOrBlank(final String string) {
+        return string != null && !string.isBlank();
+    }
+}

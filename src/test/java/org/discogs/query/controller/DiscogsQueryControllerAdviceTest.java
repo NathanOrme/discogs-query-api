8fdf4ddde69bfac974f803de91acde35b0b46b20
Nathan Orme <nathan.orme23@gmail.com>
2024-09-11 14:00:08 +0100

    Tests

--- src/test/java/org/discogs/query/controller/DiscogsQueryControllerAdviceTest.java
+++ src/test/java/org/discogs/query/controller/DiscogsQueryControllerAdviceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,5 +66,35 @@ class DiscogsQueryControllerAdviceTest {
         assertEquals(HttpStatus.REQUEST_TIMEOUT, response.getStatusCode());
         assertNotNull(response.getBody());
         assertEquals("Request took too long to process.", response.getBody().getErrorMessage());
+    }
+
+    @Test
+    void testHandleIOException() {
+        // Arrange
+        IOException exception = new IOException("IO error occurred");
+
+        // Act
+        ResponseEntity<ErrorMessageDTO> response = controllerAdvice.handleIOException(exception);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("IO error occurred", response.getBody().getErrorMessage());
+    }
+
+    @Test
+    void testHandleGenericException() {
+        // Arrange
+        Exception exception = new Exception("Generic error occurred");
+
+        // Act
+        ResponseEntity<ErrorMessageDTO> response = controllerAdvice.handleException(exception);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("An unexpected error occurred: Generic error occurred", response.getBody().getErrorMessage());
     }
 }

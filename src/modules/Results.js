1a1dd8164eb535e5c75502103f62af556168a54c
Nathan Orme <nathan.orme23@gmail.com>
2024-09-26 12:57:09 +0100

    added button

--- src/modules/Results.js
+++ src/modules/Results.js
@@ -1,6 +1,16 @@
-// src/modules/Results.js
-
 import React from 'react';
+
+// Function to download JSON data
+const exportToJson = (data, filename) => {
+  const jsonString = `data:text/json;charset=utf-8,${encodeURIComponent(
+    JSON.stringify(data, null, 2)
+  )}`;
+  const link = document.createElement('a');
+  link.href = jsonString;
+  link.download = `${filename}.json`;
+
+  link.click();
+};
 
 export const displayError = (message) => {
   return <p className="error-message">{message}</p>;
@@ -83,6 +93,12 @@ const Results = ({ response }) => {
 
   return (
     <div id="results">
+      <button
+        onClick={() => exportToJson(response, 'results')}
+        className="export-button"
+      >
+        Export Results to JSON
+      </button>
       {renderResults()}
     </div>
   );

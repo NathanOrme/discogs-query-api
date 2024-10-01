7b650fc150aec4c69f7f565a9f4ff72e66e15493
NathanOrme <NathanOrme@users.noreply.github.com>
2024-10-01 08:28:38 +0000

    style: format files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // src/App.js
-import React, { useState } from 'react';
-import './css/base.css';
-import QueryFields from './modules/QueryFields';
-import SearchForm from './modules/SearchForm';
-import DarkModeToggle from './modules/DarkModeToggle';
-import CheapestItem from './modules/CheapestItem';
-import Results from './modules/Results';
+import React, { useState } from "react";
+import "./css/base.css";
+import QueryFields from "./modules/QueryFields";
+import SearchForm from "./modules/SearchForm";
+import DarkModeToggle from "./modules/DarkModeToggle";
+import CheapestItem from "./modules/CheapestItem";
+import Results from "./modules/Results";
 
 function App() {
   const [queries, setQueries] = useState([{ id: 1 }]);
@@ -23,39 +23,50 @@ function App() {
         <DarkModeToggle />
         <div class="instructions">
           <h2>How to Use This Search Tool</h2>
-          <p>This tool allows you to search for music releases from the Discogs
-            database. Follow these steps to get
-            started:</p>
-          <p><strong>Note that you must supply at least the barcode or the artist</strong></p>
+          <p>
+            This tool allows you to search for music releases from the Discogs
+            database. Follow these steps to get started:
+          </p>
+          <p>
+            <strong>
+              Note that you must supply at least the barcode or the artist
+            </strong>
+          </p>
           <ol>
-            <li><strong>Barcode: </strong>Enter the barcode for the product you are searching for.</li>
-            <li><strong>Artist:</strong> Enter the name of the artist you are
+            <li>
+              <strong>Barcode: </strong>Enter the barcode for the product you
+              are searching for.
+            </li>
+            <li>
+              <strong>Artist:</strong> Enter the name of the artist you are
               searching for.
             </li>
-            <li><strong>Track:</strong> Enter the name of the track. This is
-              optional, so you can leave it blank if not
-              needed.
+            <li>
+              <strong>Track:</strong> Enter the name of the track. This is
+              optional, so you can leave it blank if not needed.
             </li>
-            <li><strong>Album:</strong> Enter the name of the Album. This is
-              optional, so you can leave it blank if not
-              needed.
+            <li>
+              <strong>Album:</strong> Enter the name of the Album. This is
+              optional, so you can leave it blank if not needed.
             </li>
-            <li><strong>Format:</strong> Select the format of the release (e.g.,
+            <li>
+              <strong>Format:</strong> Select the format of the release (e.g.,
               Vinyl, Compilation). This is optional.
             </li>
-            <li><strong>Types:</strong> Select the type of release you are
-              looking for (e.g., Release, Master). This is
-              optional.
+            <li>
+              <strong>Types:</strong> Select the type of release you are looking
+              for (e.g., Release, Master). This is optional.
             </li>
-            <li><strong>Country:</strong> Select the country of release you are
-              looking for (e.g., UK, US). This is
-              optional.
+            <li>
+              <strong>Country:</strong> Select the country of release you are
+              looking for (e.g., UK, US). This is optional.
             </li>
-            <li>Click the <strong>Add Another Query</strong> button to add more
-              search criteria. Each query will be
-              processed separately.
+            <li>
+              Click the <strong>Add Another Query</strong> button to add more
+              search criteria. Each query will be processed separately.
             </li>
-            <li>Click the <strong>Search</strong> button to submit your queries
+            <li>
+              Click the <strong>Search</strong> button to submit your queries
               and see the results.
             </li>
           </ol>
@@ -76,7 +87,9 @@ function App() {
         <div className="cheapest-item-section">
           <CheapestItem items={cheapestItems} />
         </div>
-        <div id="loading" style={{ display: 'none' }}>Loading...</div>
+        <div id="loading" style={{ display: "none" }}>
+          Loading...
+        </div>
       </main>
     </div>
   );

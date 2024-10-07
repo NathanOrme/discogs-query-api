e7d3042d582cdac94e92396ad4572d9c6024b473
Nathan Orme <nathan.orme23@gmail.com>
2024-10-07 19:32:51 +0100

    Convert JS to TS

--- src/main/frontend/src/modules/SearchForm.tsx
+++ src/main/frontend/src/modules/SearchForm.tsx
@@ -41,9 +41,6 @@ const SearchForm: React.FC<SearchFormProps> = ({
   const getApiUrl = (): string => {
     const hostname = window.location.hostname;
     const urlMapping: Record<string, string> = {
-      render: "https://discogs-query-api.onrender.com/discogs-query/search",
-      koyeb: "https://discogs-query-api-rgbnathan.koyeb.app/discogs-query/search",
-      b4a: "https://discogsqueryapi1-fthsfv0p.b4a.run/discogs-query/search",
       netlify: "https://discogsqueryapi1-fthsfv0p.b4a.run/discogs-query/search",
     };
 

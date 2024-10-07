4a24c0d6b04c63fab5f6cfd1794d0f76ed3148b7
Nathan Orme <nathan.orme23@gmail.com>
2024-10-07 11:27:28 +0100

    Added jsdocs

--- src/main/frontend/src/modules/SearchForm.js
+++ src/main/frontend/src/modules/SearchForm.js
@@ -2,15 +2,28 @@
 
 import React, { useState } from "react";
 
+/**
+ * SearchForm component for submitting queries and handling responses.
+ *
+ * @param {Object} props - The component props.
+ * @param {Array} props.queries - The queries to be submitted.
+ * @param {Function} props.setResponse - Function to set the response data.
+ * @param {Function} props.onCheapestItemsChange - Function to handle changes in the cheapest items.
+ * @returns {JSX.Element} The SearchForm component.
+ */
 const SearchForm = ({ queries, setResponse, onCheapestItemsChange }) => {
   const [loading, setLoading] = useState(false);
 
+  /**
+   * Gets the API URL based on the current hostname.
+   *
+   * @returns {string} The API URL.
+   */
   const getApiUrl = () => {
     const hostname = window.location.hostname;
     const urlMapping = {
       render: "https://discogs-query-api.onrender.com/discogs-query/search",
-      koyeb:
-        "https://discogs-query-api-rgbnathan.koyeb.app/discogs-query/search",
+      koyeb: "https://discogs-query-api-rgbnathan.koyeb.app/discogs-query/search",
       b4a: "https://discogsqueryapi1-fthsfv0p.b4a.run/discogs-query/search",
       netlify: "https://discogsqueryapi1-fthsfv0p.b4a.run/discogs-query/search",
     };
@@ -24,6 +37,11 @@ const SearchForm = ({ queries, setResponse, onCheapestItemsChange }) => {
     return "http://localhost:9090/discogs-query/search";
   };
 
+  /**
+   * Handles the submission of the search form.
+   *
+   * @param {Event} event - The form submit event.
+   */
   const handleSearchFormSubmit = (event) => {
     event.preventDefault();
     setLoading(true);
@@ -44,7 +62,7 @@ const SearchForm = ({ queries, setResponse, onCheapestItemsChange }) => {
         if (!response.ok) {
           return response.text().then((errorMessage) => {
             throw new Error(
-              `Server responded with status ${response.status}: ${errorMessage}`,
+              `Server responded with status ${response.status}: ${errorMessage}`
             );
           });
         }

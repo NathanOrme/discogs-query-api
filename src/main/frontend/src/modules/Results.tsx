f4de01c3284c7fb2ef7b478d76cd0180801efdca
Nathan Orme <nathan.orme23@gmail.com>
2024-11-23 17:52:22 +0000

    Code fix

--- src/main/frontend/src/modules/Results.tsx
+++ src/main/frontend/src/modules/Results.tsx
@@ -1,13 +1,11 @@
-// src/modules/Results.tsx
-
 import React from 'react';
 import exportToJson from './utils/exportToJson';
 import QueryResults from './results/QueryResults';
 import ErrorMessage from './utils/ErrorMessage';
-import Entry from './results/Entry';
+import EntryComponent, { EntryData } from './results/Entry';
 
 interface QueryResult {
-  results: Record<string, Entry[]>;
+  results: Record<string, EntryData[]>;
 }
 
 interface ResultsProps {

a2a1f3f1454f2fa5a3f66c32615bf718646f9276
Nathan <nathan.orme23@gmail.com>
2024-11-24 10:23:15 +0000

    Viject (#158)
    * Initial conversion
    
    * Updated config
    
    * Formatted files
    
    * Fixed yarn

---
+++ src/main/frontend/jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+};

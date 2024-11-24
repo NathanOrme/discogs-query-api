194c5b2e711fb8189fe37ee8c45dd95b4344710f
Nathan Orme <nathan.orme23@gmail.com>
2024-11-24 10:09:26 +0000

    Updated config

---
+++ src/main/frontend/jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleNameMapper: {
+      '^.+\\.(css|scss|sass)$': 'identity-obj-proxy'
+    }
+  };
+  

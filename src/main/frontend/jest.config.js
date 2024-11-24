2382b61fb0e21b84facba07cf191abb22847d702
Nathan Orme <nathan.orme23@gmail.com>
2024-11-24 10:11:04 +0000

    Formatted files

--- src/main/frontend/jest.config.js
+++ src/main/frontend/jest.config.js
@@ -1,12 +1,11 @@
 // jest.config.js
 module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-      '^.+\\.tsx?$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '^.+\\.(css|scss|sass)$': 'identity-obj-proxy'
-    }
-  };
-  
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^.+\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+};

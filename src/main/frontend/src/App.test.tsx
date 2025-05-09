9a79b0a9bd3fb983df2e54603922ccd09bd3d73c
Nathan Orme <nathan.orme23@gmail.com>
2025-05-09 10:33:29 +0100

    delete test

--- src/main/frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header and next button', () => {
-  render(<App />);
-  expect(screen.getByText(/Discogs Query App/i)).toBeInTheDocument();
-  expect(screen.getByRole('button', { name: /Next/i })).toBeInTheDocument();
-});
